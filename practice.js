1969374216862070651f523ad22d4d0f46e3d091
Truman Proctor <trumanproctor@Trumans-MacBook-Pro.local>
2018-11-12 00:00:49 -0700

    1.2 JS Arrays Assignment

--- practice.js
+++ practice.js
@@ -9,7 +9,7 @@
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
-var arr = [10,20,30];
+var arr = [10, 20, 30];
 // Do not edit the code above.
 
 /*
@@ -17,14 +17,15 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
-var arr = [40,50,60];
+var arr = [40, 50, 60];
 // Do not edit the code above.
 
 /*
@@ -32,8 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+function last(arr) {
+  return arr[2];
+};
 
 
 ////////// PROBLEM 3 //////////
@@ -47,8 +49,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family) {
+  for (i = 0; i < family.length; i++)
+    alert(family[i]);
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -62,14 +66,18 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters) {
+  for (i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+};
 
 
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var nums = [1,2,3,6,22,98,45,23,22,12];
+var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
 // Do not edit the code above.
 
 /*
@@ -77,225 +85,231 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums) {
+  var evensArray = [];
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0) {
+      evensArray.push(nums[i]);
+    };
+    };
+    return evensArray;
+  };
 
 
 
 
 
+    /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
+    ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
 
-/////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
-////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
 
 
 
 
+    ////////// PROBLEM 6 //////////
 
+    // Do not edit the code below.
+    var numbersArray = [1, 2, 34, 54, 55, 34, 32, 11, 19, 17, 54, 66, 13];
+    // Do not edit the code above.
 
-////////// PROBLEM 6 //////////
+    /*
+      Write a function called divider that is given one argument, numbersArray.
+      Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
+    */
 
-// Do not edit the code below.
-var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
-// Do not edit the code above.
+    //Code Here
 
-/*
-  Write a function called divider that is given one argument, numbersArray.
-  Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
-*/
 
-//Code Here
 
+    ////////// PROBLEM 7 //////////
 
+    // Do not edit the code below.
+    var getRandomArbitrary = function () {
+      return Math.floor(Math.random() * 30);
+    };
+    // Do not edit the code above.
 
-////////// PROBLEM 7 //////////
+    /* 
+      var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+      Above you're given a function that will return a random number between 0 and 30. There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+      Write a function named finder that will take in an array as an argument.
+      It will then  get a random number (by invoking getRandomArbitrary).
+      Loop through the array to see if that random number is in the array. 
+      If it is, return true, if it's not, return false
+    */
 
-// Do not edit the code below.
-var getRandomArbitrary = function() {
-  return Math.floor(Math.random() * 30);
-};
-// Do not edit the code above.
+    //Code Here
 
-/* 
-  var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-  Above you're given a function that will return a random number between 0 and 30. There is also a commented out array full of numbers to help you visualize what your function will be receiving.
-  Write a function named finder that will take in an array as an argument.
-  It will then  get a random number (by invoking getRandomArbitrary).
-  Loop through the array to see if that random number is in the array. 
-  If it is, return true, if it's not, return false
-*/
 
-//Code Here
 
+    ////////// PROBLEM 8 //////////
 
+    // Do not edit the code below.
+    var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+    // Do not edit the code above.
 
-////////// PROBLEM 8 //////////
+    /*
+      Here we're going to write a function that mimics going shopping and checking things off of our grocery list and adding new items to our list.
+    
+      Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
+      If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
+    
+      Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
+      In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
+    
+      In both removeItem and addItem check to see if the 'myGroceryList' and 'item' arguments are truthy.
+      If they are not, return an empty array.
+    
+      Here are some examples of calling your functions and what should be returned:
+      removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+      addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
+      removeItem(myGroceryList) --> [];
+      addItem() --> [];
+    */
 
-// Do not edit the code below.
-var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-// Do not edit the code above.
+    //Code Here
 
-/*
-  Here we're going to write a function that mimics going shopping and checking things off of our grocery list and adding new items to our list.
 
-  Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
-  If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
 
-  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
-  In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
+    ////////// PROBLEM 9 //////////
 
-  In both removeItem and addItem check to see if the 'myGroceryList' and 'item' arguments are truthy.
-  If they are not, return an empty array.
+    /*
+      Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
+    */
 
-  Here are some examples of calling your functions and what should be returned:
-  removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-  addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
-  removeItem(myGroceryList) --> [];
-  addItem() --> [];
-*/
+    //Code Here
 
-//Code Here
 
 
+    ////////// PROBLEM 10 //////////
 
-////////// PROBLEM 9 //////////
+    // Do not edit the code below.
+    var numbers = [5, '9', 16, 19, '25', '34', 48];
+    // Do not edit the code above.
 
-/*
-  Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
-*/
+    /*
+      Write a function called addTen that is given 'numbers' as it's only argument.
+      Return a new array after adding ten to each item in numbers. 
+      *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
+    */
 
-//Code Here
+    //Code Here
 
 
 
-////////// PROBLEM 10 //////////
+    ////////// PROBLEM 11 //////////
 
-// Do not edit the code below.
-var numbers = [5, '9', 16, 19, '25', '34', 48];
-// Do not edit the code above.
+    // Do not edit the code below.
+    var num1 = Math.floor(Math.random() * 30);
+    var num2 = Math.floor(Math.random() * 30);
+    var arr1 = [];
+    var arr2 = [];
+    for (var i = 0; i < num1; i++) {
+      arr1.push(i);
+    }
+    for (var i = 0; i < num2; i++) {
+      arr2.push(i);
+    }
+    // Do not edit the code above.
 
-/*
-  Write a function called addTen that is given 'numbers' as it's only argument.
-  Return a new array after adding ten to each item in numbers. 
-  *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
-*/
-  
-//Code Here
+    /*
+      Above is some code that adds a random number of values to both arr1 and arr2.
+      Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. 
+      Return the array which is longest.
+    */
 
+    //Code Here
 
 
-////////// PROBLEM 11 //////////
 
-// Do not edit the code below.
-var num1 = Math.floor(Math.random() * 30);
-var num2 = Math.floor(Math.random() * 30);
-var arr1 = [];
-var arr2 = [];
-for(var i = 0; i < num1; i++){
-  arr1.push(i);
-}
-for(var i = 0; i < num2; i++){
-  arr2.push(i);
-}
-// Do not edit the code above.
+    /*
+      As a continuation of the previous problem, write another function called 'both'.
+      Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
+      'both' should return a new array with the matching numbers found in both arr1 and arr2.
+    
+      Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
+    */
 
-/*
-  Above is some code that adds a random number of values to both arr1 and arr2.
-  Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. 
-  Return the array which is longest.
-*/
+    //Code Here
 
-//Code Here
 
 
+    ////////// PROBLEM 12 //////////
 
-/*
-  As a continuation of the previous problem, write another function called 'both'.
-  Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
-  'both' should return a new array with the matching numbers found in both arr1 and arr2.
+    // Do not edit the code below.
+    var devMountainEmployees = [];
 
-  Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
-*/
+    var tyler = {
+      name: 'Tyler',
+      position: 'Lead Instructor/Engineer',
+      spiritAnimal: 'Honey Badger'
+    };
 
-//Code Here
+    var cahlan = {
+      name: 'Cahlan',
+      position: 'CEO',
+      spiritAnimal: 'butterfly'
+    };
 
+    var ryan = {
+      name: 'Ryan',
+      position: 'Marketing',
+      spiritAnimal: 'fox'
+    };
 
+    var colt = {
+      name: 'Colt',
+      position: 'Everything really',
+      spiritAnimal: 'Young Male Horse'
+    };
+    // Do not edit the code above.
 
-////////// PROBLEM 12 //////////
+    /*
+      Above you're given an empty array and four variables containing objects. 
+      Fill the devMountainEmployees array with those four objects. 
+      After that console.log the length of the Array and make sure that it's equal to 4. 
+    */
 
-// Do not edit the code below.
-var devMountainEmployees = [];
+    //Code Here
 
-var tyler = {
-    name: 'Tyler',
-    position: 'Lead Instructor/Engineer',
-    spiritAnimal: 'Honey Badger'
-};
 
-var cahlan = {
-    name: 'Cahlan',
-    position: 'CEO',
-    spiritAnimal: 'butterfly'
-};
 
-var ryan = {
-    name: 'Ryan',
-    position: 'Marketing',
-    spiritAnimal: 'fox'
-};
+    /*
+      Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
+      Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
+    */
 
-var colt = {
-    name: 'Colt',
-    position: 'Everything really',
-    spiritAnimal: 'Young Male Horse'
-};
-// Do not edit the code above.
+    //Code Here
 
-/*
-  Above you're given an empty array and four variables containing objects. 
-  Fill the devMountainEmployees array with those four objects. 
-  After that console.log the length of the Array and make sure that it's equal to 4. 
-*/
 
-//Code Here
 
+    ////////// PROBLEM 13 //////////
 
 
-/*
-  Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
-  Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
-*/
+    /*
+      A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects) of Data is to have an Array full of objects. 
+      Create an empty array called users.
+    */
 
-//Code Here
+    //Code Here
 
 
 
-////////// PROBLEM 13 //////////
+    /*
+      Now add three user objects to your users array. Each user object should contain the following properties. name, email, password, username.
+    
+      Include the following user1 object as one of the objects in your array.
+    */
 
-
-/*
-  A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects) of Data is to have an Array full of objects. 
-  Create an empty array called users.
-*/
-
-//Code Here
-
-
-
-/*
-  Now add three user objects to your users array. Each user object should contain the following properties. name, email, password, username.
-
-  Include the following user1 object as one of the objects in your array.
-*/
-
-// Do not edit the code below.
-var user1 = {
-    name: 'Tyler McGinnis',
-    email: 'tylermcginnis33@gmail.com',
-    password: 'iLoveJavaScript',
-    username: 'infiniteLoop'
-};
+    // Do not edit the code below.
+    var user1 = {
+      name: 'Tyler McGinnis',
+      email: 'tylermcginnis33@gmail.com',
+      password: 'iLoveJavaScript',
+      username: 'infiniteLoop'
+    };
 // Do not edit the code above.
 
 //Code Here
